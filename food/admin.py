783e73ff9fd6f42fa9953cd4de59d466d3e67202
kiri3914 <t.bektur.2002@gmail.com>
2021-09-28 21:33:42 +0600

    d

---
+++ food/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Starter)
+admin.site.register(Salad)
+admin.site.register(Specialty)
+admin.site.register(Table)
+admin.site.register(Program)
+admin.site.register(Response)
+
+
+
+
