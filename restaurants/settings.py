6c0cd0d86ee97d4881a316d345f61cff685650f2
kiri3914 <t.bektur.2002@gmail.com>
2021-09-30 15:24:40 +0600

    sd

--- restaurants/settings.py
+++ restaurants/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9sjwyt79@ubyf(j(-g$vs3$el7%mk0(#=embw!-#ea0_+mg$xf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'restaurants.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'elfido',
+        'USER': 'res',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
