3a9db5e1cfb70780dcbe3d48074d64cd8b6c9549
kiri3914 <you@example.com>
2021-11-15 01:15:20 +0600

    rjytt

--- restaurants/settings.py
+++ restaurants/settings.py
@@ -75,21 +75,14 @@ WSGI_APPLICATION = 'restaurants.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 #
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'elfido',
-#         'USER': 'res',
-#         'PASSWORD': '123',
-#         'HOST': 'localhost',
-#         'PORT': '5432'
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'elfido_db',
+        'USER': 'elfido',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
